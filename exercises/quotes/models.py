9622748f0b7d781d810590c3620cc2f822727a89
Liamdelee <liam.delee@student.ap.be>
2017-09-07 16:41:12 +0200

    adding mongo db save

--- exercises/quotes/models.py
+++ exercises/quotes/models.py
@@ -4,9 +4,9 @@ from mongoengine import *
 
 class Recept(Document):
     recept_name = StringField(max_length=50)
-    recept_aantalCalorien = StringField(max_length=50)
+    recept_aantalCalorien = IntField()
     recept_ingredienten = StringField(max_length=50)
-    recept_benodigdeTijd = StringField(max_length=50)
+    recept_benodigdeTijd = IntField()
 
     def __str__(self):
         return self.name
--- exercises/quotes/views.py
+++ exercises/quotes/views.py
@@ -5,7 +5,7 @@ from mongoengine import *
 connect('test', host='mongodb://localhost/test')
 
 def list_all_recepten(request):
-    recepten = Recept.objects.all()
+    recepten = Recept.objects
     context = {'recepten': recepten}
     return render(request, 'quotes/index.html', context)
 
@@ -24,9 +24,20 @@ def list_all_recepten(request):
 
 def add(request):
     if request.method == 'POST':
+        recept = request.POST.get('recept').title()
+        receptsplit = recept.split(",")
+        recept_name = receptsplit[0]
+        recept_aantalCalorien = int(receptsplit[1])
+        recept_ingredienten = receptsplit[2]
+        recept_benodigdeTijd = int(receptsplit[3])
         try:
-            recept = Recept(recept_name = "test", recept_aantalCalorien = "test", recept_ingredienten = "test", recept_benodigdeTijd = "test").save()
-            return render(request, 'quotes/ok.html', None)
+            #Recept.Objects(recept_name____exact=recept_name) geen idee hoe vergelijken
+            for recept in Recept.objects:
+                if recept.recept_name == recept_name:
+                    return render(request, 'quotes/add.html', None)
+                else:
+                    recept = Recept(recept_name = recept_name, recept_aantalCalorien = recept_aantalCalorien, recept_ingredienten = recept_ingredienten, recept_benodigdeTijd = recept_benodigdeTijd).save()
+                    return render(request, 'quotes/ok.html', None)
         except:
             return render(request, 'quotes/index.html', None)
     else:
