ea2a48a2eda4187a3ac17f46907437a023b838d8
Liamdelee <liam.delee@student.ap.be>
2017-09-07 16:50:07 +0200

    almost final edit

--- exercises/quotes/views.py
+++ exercises/quotes/views.py
@@ -5,22 +5,9 @@ from mongoengine import *
 connect('test', host='mongodb://localhost/test')
 
 def list_all_recepten(request):
-    recepten = Recept.objects
+    recepten = Recept.objects.order_by('recept_name', 'recept_aantalCalorien')
     context = {'recepten': recepten}
     return render(request, 'quotes/index.html', context)
-
-#def search(request):
-#    if request.method == 'POST':
-#        author_name = request.POST.get('name').title() # Capitalize name
-#        try:
-#            author = Author.objects.get(name=author_name)
-#            quotes = Quote.objects.filter(author_id=author.id)
-#            context = {'quotes': quotes}
-#            return render(request, 'quotes/index.html', context)
-#        except:
-#            return render(request, 'quotes/index.html', None)
-#    else:
-#        return render(request, 'quotes/search.html', None)
 
 def add(request):
     if request.method == 'POST':
